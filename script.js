0cd6ca6dda13c921e5f0a4ebf8fd37837d85685f
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2023-11-18 19:39:28 -0800

    All the html and css of the site should be ready now

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 let hamburgerMenu = document.querySelector(".hamburger")
-let linksMenu = document.querySelector(".menu")
+let mobileMenu = document.querySelector(".mobile-menu")
 let closeMenu = document.querySelector(".close-menu")
 let overlay = document.querySelector(".overlay")
+let nav = document.querySelector("nav")
+let logo = document.querySelector(".logo")
+
 hamburgerMenu.addEventListener("click", () => {
-  linksMenu.classList.add("open")
-  document.body.style.overflowY = "hidden"
+  mobileMenu.classList.add("open")
+  mobileMenu.style.overflowY = "auto"
   overlay.style.display = "block"
+  document.body.style.overflowY = "hidden"
 })
 
 closeMenu.addEventListener("click", () => {
-  linksMenu.classList.remove("open");
-  document.body.style.overflowY = "auto"
+  mobileMenu.classList.remove("open");
   overlay.style.display = "none"
+  document.body.style.overflowY = "auto"
 
 })
-/**
- * <svg
-xmlns="http://www.w3.org/2000/svg"
-x="0px" y="0px"
-width="50" height="50"
-viewBox="0 0 172 172"
-style=" fill:#26e07f;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#1fb141"><path d="M21.5,21.5v129h64.5v-32.25v-64.5v-32.25zM86,53.75c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25c-17.7805,0 -32.25,14.4695 -32.25,32.25zM118.25,86c-17.7805,0 -32.25,14.4695 -32.25,32.25c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25z"></path></g></g></svg>
- */
-// let fs = require('fs')
 
-// fs.readdir((err, files) => {
-//   if(err){
-//     console.log(err)
-//   }
-//   console.log(files)
-// })
+document.body.addEventListener("click", (event) => {
+  let closeBtnbool = event.target.closest(".close-menu")
+  let openBtnbool = event.target.closest(".hamburger")
+  if(closeBtnbool === null && openBtnbool === null){
+    mobileMenu.classList.remove("open")
+    overlay.style.display = "none"
+    document.body.style.overflowY = "auto"
+    console.log("not buttons")
+  }
 
+  
+})
 
-// function handleClick(){
-//   linksMenu.style.transform = "translate(100%)"
-//   linksMenu.style.display = "none"
-// }
+window.addEventListener("scroll", () => {
+  if(scrollY > 0){
+    nav.classList.add("sticky")
+    nav.style.background = "white"
 
-// if(closeMenu){
-//   closeMenu.addEventListener("click", handleClick)
-// }else {
-//   closeMenu.removeEventListener("click", handleClick)
-// }
-
-// console.log(closeMenu)
+  }else{
+    nav.classList.remove("sticky")
+    nav.style.background = "transparent"
+  }
+})
